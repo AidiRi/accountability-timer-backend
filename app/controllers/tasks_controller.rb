77546e974ba0640ea95bd3ba6a6e3daa328ad765
AidiRi <rivera.aidi@gmail.com>
2019-07-26 10:27:38 -0700

    Setup all controllers & CORS

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,30 @@
 class TasksController < ApplicationController
+
+  def index
+    tasks = Task.all
+    render json: tasks
+  end
+
+  def create
+    task = Task.create(task_params)
+
+    render json: task
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+
+    render json: task
+  end
+
+	def destroy
+		Task.destroy(params[:id]);
+	end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :status, :work_session_id, :authenticity_token)
+  end
 end
--- app/controllers/work_sessions_controller.rb
+++ app/controllers/work_sessions_controller.rb
@@ -1,2 +1,28 @@
 class WorkSessionsController < ApplicationController
+
+  def index
+    work_sessions = WorkSession.all
+    render json: work_sessions
+  end
+
+  def create
+    work_session = WorkSession.create(work_session_params)
+    render json: work_session
+  end
+
+  def update
+    work_session = WorkSession.find(params[:id])
+    work_session.update(work_session_params)
+  end
+
+  def destroy
+    WorkSession.destroy(params[:id])
+  end
+
+  private
+
+    def work_session_params
+      params.require(:work_session).permit(:start_time, :end_time, :total_time, :note, :authenticity_token)
+    end
+
 end
